e80fd4eb8a1bef372ed2dbcc78494ee7d9b21400
pai9nka <pai9nka@gmail.com>
2019-02-14 21:50:58 +0700

    Commit message.

---
+++ app/src/main/java/com/example/sensors/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.sensors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.List;
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    TextView tvText;
+    SensorManager sensorManager;
+    List<Sensor> sensors;
+    Sensor sensorLight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvText = (TextView) findViewById(R.id.tvText);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        sensorLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    public void onClickSensList(View v) {
+
+        sensorManager.unregisterListener(listenerLight, sensorLight);
+        StringBuilder sb = new StringBuilder();
+
+        for (Sensor sensor : sensors) {
+            sb.append("name = ").append(sensor.getName())
+                    .append(", type = ").append(sensor.getType())
+                    .append("\nvendor = ").append(sensor.getVendor())
+                    .append(" ,version = ").append(sensor.getVersion())
+                    .append("\nmax = ").append(sensor.getMaximumRange())
+                    .append(", resolution = ").append(sensor.getResolution())
+                    .append("\n--------------------------------------\n");
+        }
+        tvText.setText(sb);
+    }
+
+    public void onClickSensLight(View v) {
+        sensorManager.registerListener(listenerLight, sensorLight,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(listenerLight, sensorLight);
+    }
+
+    SensorEventListener listenerLight = new SensorEventListener() {
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            tvText.setText(String.valueOf(event.values[0]));
+        }
+    };
+}
+
+
+
+/*
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+<?xml version="1.0" encoding="utf-8"?>
+<android.support.constraint.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context=".MainActivity"/>
+
+*/
